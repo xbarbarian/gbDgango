6f7f49714a2d063838508970d44236c33153b3b9
xBarbarian <alexadr.jukov@gmail.com>
2021-03-04 15:04:30 +0300

    1 to 5 task done.

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from mainapp.models import Product, ProductCategory
+
+
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductCategory)
--- mainapp/models.py
+++ mainapp/models.py
@@ -7,6 +7,12 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Product Categories'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -16,6 +22,9 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_img', blank=True)
     category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
 
 
 
