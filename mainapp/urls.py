21baba7b95701ee8aec7144bc937e6f1e04972d7
xBarbarian <alexadr.jukov@gmail.com>
2021-03-22 21:15:15 +0300

    add list category to products.html

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 # from mainapp.views import products, single_product
-from mainapp.views import ProductListView, ProductCategoryListView
+from mainapp.views import ProductListView
 
 app_name = 'mainapp'
 
 urlpatterns = [
    path('', ProductListView.as_view(), name='index'),
-   path('<int:pk>/', ProductListView.as_view(), name='product'),
+   # path('<int:pk>/', .as_view(), name='product'),
    # path('<int:product_id>/', single_product, name='single_product'),
    # path('page/<int:page>/', products, name='page'),
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -14,45 +14,18 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-# def products(request, category_id=None, page=1):
-#     context = {
-#         'title': 'GeekShop - Каталог',
-#         # 'products': Product.objects.all(),
-#         'categories': ProductCategory.objects.all()
-#     }
-#     if category_id:
-#         products = Product.objects.filter(category_id=category_id)
-#         # context.update({'products': Product.objects.filter(category_id=category_id)})
-#     else:
-#         products = Product.objects.all()
-#         # context.update({'products': Product.objects.all()})
-#
-#     paginator = Paginator(products, per_page=3)
-#     products_paginator = paginator.page(page)
-#
-#     context.update({'products': products_paginator})
-#     return render(request, 'mainapp/products.html', context)
-
-
-# def single_product(request, id):
-#     context = {
-#         'product': Product.objects.get(id=id)
-#     }
-#     return render(request, 'mainapp/product.html', context)
-
-
 class ProductListView(ListView):
     paginate_by = 2
     model = Product
     template_name = 'mainapp/products.html'
+    context_object_name = 'products'
+
+    # def get_queryset(self):
 
     def get_context_data(self, **kwargs):
         context = super(ProductListView, self).get_context_data(**kwargs)
         context['title'] = 'GeekShop - Каталог'
-        context['category'] = Product.objects.all()
+        context['categories'] = ProductCategory.objects.all()
         return context
 
 
-class ProductCategoryListView(ListView):
-    model = Product
-    template_name = 'mainapp/products.html'
