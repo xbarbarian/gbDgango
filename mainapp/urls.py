fbd601c8132855623d9df91677c9ea9ab53cf2d9
xBarbarian <alexadr.jukov@gmail.com>
2021-03-22 17:37:45 +0300

    add CBV Views for mainappp, add pagination for CBV. Have some problem to list category for CBV forrmat.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from mainapp.views import products, single_product
+# from mainapp.views import products, single_product
+from mainapp.views import ProductListView, ProductCategoryListView
 
 app_name = 'mainapp'
 
 urlpatterns = [
-   path('', products, name='index'),
-   path('<int:category_id>/', products, name='product'),
-   path('<int:product_id>/', single_product, name='single_product'),
-   path('page/<int:page>/', products, name='page'),
+   path('', ProductListView.as_view(), name='index'),
+   path('<int:pk>/', ProductListView.as_view(), name='product'),
+   # path('<int:product_id>/', single_product, name='single_product'),
+   # path('page/<int:page>/', products, name='page'),
 
 ]
