27546da5c4239a8e65e4c433cd84ca40474abd18
xBarbarian <alexadr.jukov@gmail.com>
2021-03-23 10:40:37 +0300

    Add rules by html template is_paginated for pagination, add filter for categories.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,7 +7,7 @@ app_name = 'mainapp'
 
 urlpatterns = [
    path('', ProductListView.as_view(), name='index'),
-   # path('<int:pk>/', .as_view(), name='product'),
+   path('<int:category_id>/', ProductListView.as_view(), name='product'),
    # path('<int:product_id>/', single_product, name='single_product'),
    # path('page/<int:page>/', products, name='page'),
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic.list import ListView
-from django.views.generic.detail import SingleObjectMixin
 
 from mainapp.models import Product, ProductCategory
 
@@ -20,12 +19,16 @@ class ProductListView(ListView):
     template_name = 'mainapp/products.html'
     context_object_name = 'products'
 
-    # def get_queryset(self):
-
     def get_context_data(self, **kwargs):
         context = super(ProductListView, self).get_context_data(**kwargs)
         context['title'] = 'GeekShop - Каталог'
         context['categories'] = ProductCategory.objects.all()
         return context
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        queryset = super(ProductListView, self).get_queryset()
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
 
 
